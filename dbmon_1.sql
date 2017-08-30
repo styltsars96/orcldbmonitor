dad9b1a1b13139296a3c800c60e3d989ff032a5d
styltsars96 <styltsars@gmail.com>
2017-08-30 11:35:09 +0300

    slight changes

--- dbmon_1.sql
+++ dbmon_1.sql
@@ -9,7 +9,7 @@ select  state, event#, event, sid, username from v$session where username is not
 
 !echo USERS CONNECTED--------------------------------------------------------------------------------------
 
-select count(sid) as NUMBER_OF_SESSIONS from v$session where username is not null;
+select 'Number of users: ' || count(sid) as NUMBER_OF_SESSIONS from v$session where username is not null;
 
 --All users (sessions) connected
 select sid, machine, username, server, module from v$session
